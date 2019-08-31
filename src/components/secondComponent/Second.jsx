3cb7e57821ab30f6e304b9891683084280305c13
rishav-pandey <27219302+rishav-pandey@users.noreply.github.com>
2019-08-31 19:00:11 +0530

    created initial component

---
+++ src/components/secondComponent/Second.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class Second extends Component {
+  render() {
+    return (
+      <div>
+        <h1>I'm Second Component</h1>
+      </div>
+    );
+  }
+}
+
+export default Second;
