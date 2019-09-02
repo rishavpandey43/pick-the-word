816f0148aeee99341d7a8f75f1b563b746bd30d2
rishav-pandey <27219302+rishav-pandey@users.noreply.github.com>
2019-09-02 17:29:38 +0530

    Updates

---
+++ precache-manifest.c2f36ebdb6f99b9250a339a968419f7e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6a2fc60c776adea727b46552fc692e6a",
+    "url": "/pick-the-word/index.html"
+  },
+  {
+    "revision": "897def90b27c4ebe54a3",
+    "url": "/pick-the-word/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "1e936e33e3416c3a56ce",
+    "url": "/pick-the-word/static/css/main.5b0b2959.chunk.css"
+  },
+  {
+    "revision": "897def90b27c4ebe54a3",
+    "url": "/pick-the-word/static/js/2.4fc2ae13.chunk.js"
+  },
+  {
+    "revision": "1e936e33e3416c3a56ce",
+    "url": "/pick-the-word/static/js/main.7863f123.chunk.js"
+  },
+  {
+    "revision": "20231b0c3ec15a922b83",
+    "url": "/pick-the-word/static/js/runtime~main.aa0c0ef3.js"
+  }
+]);
